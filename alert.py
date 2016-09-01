8aed0f37fb604d8bb2948ddcb68119f572bc93da
Adrian <adrian@sentinel.local>
2016-09-01 12:15:03 -0700

    adding initial file

---
+++ alert.py
@@ -0,0 +1,33 @@
+import time
+
+x = 1
+while True:
+	if x == 1:
+		print  " " 
+		print  "\n ######### " 
+		print  "\n ######### " 
+		print  "\n ######### " 
+		print  "\n ######### " 
+		print  "\n ######### " + "Do NOT Distub Please, holding for Omii"
+		print  "\n ######### " 
+		print  "\n ######### " 
+		print  "\n ######### " 
+		print  "\n ######### " 
+		print  "\n ######### " 
+		print  "\n ######### " 
+		print  "\n ######### " 
+		print  "\n ######### " 
+		time.sleep(.6)
+		x = 0
+	else:
+		print  " " 
+		print  "\n ######### " 
+		print  "\n ######### " 
+		print  "\n ######### " 
+		print  "\n ######### " 
+		print  "\n ######### " + "Do NOT Distub, holding for Omii"
+		print  "\n ######### " 
+		print  "\n ######### " 
+		time.sleep(.4)
+		x = 1
+
